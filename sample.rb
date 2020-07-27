255735ff86585514bc5c6692530995f9bbaf9e03
alioune210 <54614177+alioune210@users.noreply.github.com>
2020-07-27 15:34:33 +0900

    Update sample.rb

--- sample.rb
+++ sample.rb
@@ -3,15 +3,13 @@ require 'clockwork'
 
 require 'active_support/time'
 
-client = SlackNotify::Client.new(
-  webhook_url: "https://hooks.slack.com/services/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",
-  channel: "#général",
-  username: "webhookbot",
-  link_names: 1
-)
-client.notify("Hello There!")
+client = SlackNotify::Client.new(webhook_url: "https://hooks.slack.com/services/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")
 
+client.notify("Hello There!")
 client.notify("Ohayo")
+client.notify("Konnichiwa")
+client.notify("Konbanwa", "#madrassa")
+
 
 module Clockwork
     # Here is a Clockwork method that says, "How long does it take you to process?"
@@ -19,7 +17,7 @@ module Clockwork
     handler do |job|
        case job
        when "slack.job"
-        puts "Hello There!"
+        puts "Kind checkup"
        end
     end
     every(3.minutes, 'slack.job') 
