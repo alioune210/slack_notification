e2bb25fe5deb430f0ead773212630127ce14cfa5
Alioune <alioune210@hotmail.com>
2020-07-27 14:50:09 +0900

    update sample.rb

--- sample.rb
+++ sample.rb
@@ -4,13 +4,15 @@ require 'clockwork'
 require 'active_support/time'
 
 client = SlackNotify::Client.new(
-  webhook_url: "https://hooks.slack.com/services/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",
+  webhook_url: "https://hooks.slack.com/services/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",
   channel: "#général",
   username: "webhookbot",
-  icon_url: "http://mydomain.com/myimage.png",
-  icon_emoji: ":shipit:",
   link_names: 1
 )
+client.notify("Hello There!")
+
+client.notify("Ohayo")
+
 module Clockwork
     # Here is a Clockwork method that says, "How long does it take you to process?"
     # Write the process and settings you want to send at regular intervals with slack here
